54bce970e4dcee116d1e23d84224a759b2532a5a
Joshua Aragon <aragon@Joshuas-MacBook-Pro.local>
2023-01-28 22:57:24 -0700

    changes base url

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.static(path.join(__dirname, "build")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use("/api", index);
+app.use("/", index);
 app.get("*", (req, res) => {
-  res.send("HEY!");
+  res.send("EMPTY.... Gimme something!");
 });
 
 // catch 404 and forward to error handler
